a055558f9d70c638b84e31a1d11a8eb26838e975
Rein Appeldoorn <reinzor@gmail.com>
2018-08-22 20:48:04 +0200

    do not list enum, incompatible with older version

--- src/videowall/players/__init__.py
+++ src/videowall/players/__init__.py
@@ -1,3 +1,3 @@
 from .player_client import PlayerClient
 from .player_server import PlayerServer
-from .player_platforms import PlayerPlatform
+from .player_platforms import PlayerPlatform, get_player_platform_strings, player_platform_from_string
--- src/videowall/players/player_platforms.py
+++ src/videowall/players/player_platforms.py
@@ -1,9 +1,27 @@
 from enum import Enum
 
+from .player_exceptions import PlayerException
+
 
 class PlayerPlatform(Enum):
-    X86_64 = "x86_64"
-    RASPBERRY_PI = "raspberry_pi"
+    X86_64 = 1
+    RASPBERRY_PI = 2
 
-    def __str__(self):
-        return self.value
+
+_string_player_platform_map = {
+  "x86": PlayerPlatform.X86_64,
+  "rpi": PlayerPlatform.RASPBERRY_PI
+}
+
+
+def player_platform_from_string(string):
+    try:
+        platform = _string_player_platform_map[string]
+    except KeyError as e:
+        raise PlayerException(e)
+    else:
+        return platform
+
+
+def get_player_platform_strings():
+    return _string_player_platform_map.keys()
