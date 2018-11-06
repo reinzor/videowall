e461cdfcfa0340907687246c70dcccb6c943d998
Rein Appeldoorn <reinzor@gmail.com>
2018-11-06 19:17:31 +0100

    refactor

--- src/videowall/web_server.py
+++ src/videowall/web_server.py
@@ -1,73 +1,15 @@
 import logging
+import os.path
+import uuid
 
-#
-# from flask import Flask
-# from flask_socketio import SocketIO
-#
-from videowall.server import Server
-
-logger = logging.getLogger(__name__)
-#
-#
-# class WebServer:
-#     def __init__(self, webserver_port, media_path, base_time_offset, ip, server_broadcast_port,
-#                  server_play_broadcast_port,
-#                  server_clock_port, server_broadcast_interval, client_broadcast_port):
-#         self._server = Server(media_path, base_time_offset, ip, server_broadcast_port, server_play_broadcast_port,
-#                               server_clock_port, server_broadcast_interval, client_broadcast_port, {})
-#         self._ip = ip
-#         self._port = webserver_port
-#         self._app = Flask(__name__)
-#         self._app.config['SECRET_KEY'] = 'secret!'
-#         self._socketio = SocketIO(self._app)
-#
-#     def run(self):
-#         @self._socketio.on('connect')
-#         def on_connect():
-#             logger.info("Client connected")
-#
-#         @self._socketio.on('disconnect')
-#         def on_disconnect():
-#             logger.info("Client disconnect")
-#
-#         @self._socketio.on('test')
-#         def on_test(msg):
-#             logger.info("Test message: %s", msg)
-#
-#         try:
-#             self._socketio.run(self._app, host=self._ip, port=self._port)
-#         except KeyboardInterrupt:
-#             logger.info("KeyboardInterrupt")
-
-
-# !/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-"""Simplified chat demo for websockets.
-
-Authentication, error handling, etc are left as an exercise for the reader :)
-"""
-
-import logging
 import tornado.escape
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
-import os.path
-import uuid
+from videowall.server import Server
+
+logger = logging.getLogger(__name__)
 
 
 class WebServer(tornado.web.Application):
