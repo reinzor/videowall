741956f0cf0724af8789417b9c9e9ca3f93806d5
Rein Appeldoorn <reinzor@gmail.com>
2018-11-06 19:24:17 +0100

    more refactor

--- src/videowall/web_server.py
+++ src/videowall/web_server.py
@@ -1,6 +1,5 @@
 import logging
 import os.path
-import uuid
 
 import tornado.escape
 import tornado.ioloop
@@ -21,8 +20,9 @@ class WebServer(tornado.web.Application):
 
         self._server_web_port = server_web_port
 
+        WebSocketHandler.server = self._server
         super(WebServer, self).__init__([
-            ("/api", WebSocketHandler, {"server": self._server}),
+            ("/api", WebSocketHandler),
             ("/(.*)", tornado.web.StaticFileHandler, {
                 "path": os.path.join(os.path.dirname(__file__), "../../web/dist/"),
                 "default_filename": "index.html"
@@ -35,48 +35,11 @@ class WebServer(tornado.web.Application):
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    waiters = set()
-    cache = []
-    cache_size = 200
-    server = None
+    def initialize(self):
+        WebSocketHandler.server.play("/home/rein/Videos/big_buck_bunny_720p_30mb.mp4", True, {})
 
-    def initialize(self, server):
-        WebSocketHandler.server = server
-        server.play("/home/rein/Videos/big_buck_bunny_720p_30mb.mp4", True, {})
-        print("init")
-
-    def get_compression_options(self):
-        # Non-None enables compression with default options.
-        return {}
-
-    def open(self):
-        WebSocketHandler.waiters.add(self)
-
-    def on_close(self):
-        WebSocketHandler.waiters.remove(self)
-
-    @classmethod
-    def update_cache(cls, chat):
-        cls.cache.append(chat)
-        if len(cls.cache) > cls.cache_size:
-            cls.cache = cls.cache[-cls.cache_size:]
-
-    @classmethod
-    def send_updates(cls, chat):
-        logging.info("sending message to %d waiters", len(cls.waiters))
-        for waiter in cls.waiters:
-            try:
-                waiter.write_message(chat)
-            except:
-                logging.error("Error sending message", exc_info=True)
+    def data_received(self, chunk):
+        pass
 
     def on_message(self, message):
-        logging.info("got message %r", message)
-        parsed = tornado.escape.json_decode(message)
-        chat = {"id": str(uuid.uuid4()), "body": parsed["body"]}
-        chat["html"] = tornado.escape.to_basestring(
-            self.render_string("message.html", message=chat)
-        )
-
-        WebSocketHandler.update_cache(chat)
-        WebSocketHandler.send_updates(chat)
+        pass
